cadab1f75d7c0d0850a4c521eda7d1c34476f4b2
lucael <lucael4u@gmail.com>
2023-05-30 01:59:54 +0900

    Add files via upload

---
+++ lang.php
@@ -0,0 +1,10 @@
+<?php
+$no = rand(0,10)+1;
+?>
+<html>
+<meta charset="utf-8">
+<meta property="og:image" content="/lang/m<?=$no?>.jpg">
+<body>
+<img src="/lang/m<?=$no?>.jpg" />
+</body>
+</html>
